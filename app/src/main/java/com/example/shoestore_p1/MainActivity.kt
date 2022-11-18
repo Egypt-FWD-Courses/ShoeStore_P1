4415d838ab29fae44637209132616d5dd6dc51a6
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-11-18 11:23:52 +0200

    Add binding to MainActivity.kt

--- app/src/main/java/com/example/shoestore_p1/MainActivity.kt
+++ app/src/main/java/com/example/shoestore_p1/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.shoestore_p1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.ui.AppBarConfiguration
+import com.example.shoestore_p1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +13,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        
         setContentView(R.layout.activity_main)
+
+
     }
 }
