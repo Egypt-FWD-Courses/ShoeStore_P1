eb6e6eb80b364047656f424f22981712fe241eb5
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-11-18 17:08:06 +0200

    navigate to the welcome screen
    use safe args to send the email

--- app/src/main/java/com/example/shoestore_p1/User.kt
+++ app/src/main/java/com/example/shoestore_p1/User.kt
@@ -1,6 +1,7 @@
 package com.example.shoestore_p1
 
 import android.os.Bundle
+import android.text.Layout
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.shoestore_p1.databinding.FragmentUserBinding
 
 class User : Fragment() {
@@ -22,18 +24,23 @@ class User : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_user, container, false)
-        binding.createButton.setOnClickListener {
-            welcomeCheck()
+        binding.createButton.setOnClickListener { view: View ->
+            welcomeCheck(view)
+        }
+        binding.loginButton.setOnClickListener{ view: View ->
+            welcomeCheck(view)
         }
         return binding.root
     }
 
-    private fun welcomeCheck(){
+    private fun welcomeCheck(view: View){
         if (binding.userEmail.text.isEmpty() || binding.userPassword.text.isEmpty()){
             binding.alertLabel.visibility = View.VISIBLE
+
         }
         else {
             binding.alertLabel.visibility = View.INVISIBLE
+            view.findNavController().navigate(UserDirections.actionUserToWelcome(binding.userEmail.text.toString()))
         }
     }
 }
