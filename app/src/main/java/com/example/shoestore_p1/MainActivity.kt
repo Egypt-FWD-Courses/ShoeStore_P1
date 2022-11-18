c5be55ed4257f5941d0464a312b8596228dabf0b
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-11-18 11:19:14 +0200

    Add the navigation folder
    add configurations to activity_main.xml to open first page at the navigation graph

--- app/src/main/java/com/example/shoestore_p1/MainActivity.kt
+++ app/src/main/java/com/example/shoestore_p1/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.shoestore_p1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.ui.AppBarConfiguration
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
