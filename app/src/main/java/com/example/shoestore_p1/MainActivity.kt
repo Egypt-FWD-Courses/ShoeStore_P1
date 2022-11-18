5e48eab56eb71e3d5d8657cb595bd338a01b7ea1
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2022-11-18 11:37:08 +0200

    Set the navigation system and insert the first page

--- app/src/main/java/com/example/shoestore_p1/MainActivity.kt
+++ app/src/main/java/com/example/shoestore_p1/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.shoestore_p1.databinding.ActivityMainBinding
 
@@ -15,9 +18,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
-        
-        setContentView(R.layout.activity_main)
 
-
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestinationId) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
     }
 }
